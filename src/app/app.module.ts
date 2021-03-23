64347854ddaeabcacf9695d309e3e5718b374976
Natanael <NatanaelUF17@gmail.com>
2021-03-23 10:13:57 -0400

    Initializing proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
